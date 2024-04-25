b54d908e31988af7002e372b5688df6d263dcb38
KL <karl.w.langer@gmail.com>
2024-04-25 18:04:01 +0930

    Added webpack configuration, database handling for indexedDB

---
+++ client/src/sw.js
@@ -0,0 +1,32 @@
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+// Import the expiration plugin
+import { ExpirationPlugin } from 'workbox-expiration';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Register route for caching dynamic CSS and JS files.
+// i.e. bootstrap, jQuery, ...
+// The StaleWhileRevalidate strategy serves content from cache AND loads it from source if needed.
+registerRoute(
+  ({ request }) => {
+    console.log(request);
+    return (
+      // CSS
+      request.destination === 'style' ||
+      // JavaScript
+      request.destination === 'script'
+    );
+  },
+  new StaleWhileRevalidate({
+    cacheName: 'static-resources',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,62 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest(
+        // TODO: Create a manifest.json:
+        {
+        "short_name": "JATE",
+        "name": "Just Another Text Editor",
+        "icons": [
+          {
+            "src": "favicon.ico",
+            "type": "image/png",
+            "sizes": "128x128",
+            "purpose": "any maskable"
+          },
+          {
+            "src": "src/images/logo.png",
+            "type": "image/png",
+            "sizes": "512x512",
+            "purpose": "any maskable"
+          }
+        ],
+        "orientation": "portrait",
+        "display": "standalone",
+        "start_url": "./",
+        "description": "Take notes with Javascript syntax highlighting!",
+        "background_color": "#7eb4e2",
+        "theme_color": "#7eb4e2"
+      }),
+    ],  
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
       ],
     },
   };
